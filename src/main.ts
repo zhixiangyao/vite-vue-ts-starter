dbbd37dddb5abc0e3744e73ce43f85dc7b65b974
xiaojun996 <blueskyzx@icloud.com>
2021-03-10 14:58:17 +0800

    refactor

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from '/@/App.vue'
 import router from '/@/router'
 import { store, key } from '/@/store'
 import '@virtual/windi.css'
-import '/@/main.css'
+import '/@/assets/main.css'
 
 const app = createApp(App)
 
