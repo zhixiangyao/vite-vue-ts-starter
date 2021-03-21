22c46399419418a7cd83438a0c6f2ba81b6c9929
xiaojun996 <blueskyzx@icloud.com>
2021-03-21 12:34:40 +0800

    refactor

--- src/views/TensileColumn.vue
+++ src/views/TensileColumn.vue
@@ -25,57 +25,88 @@ export default defineComponent({
 <style lang="postcss" scoped>
 .column {
   @apply flex justify-between w-full h-full overflow-hidden;
-}
 
-.column-left {
-  height: calc(100vh - 64px);
+  .column-left {
+    height: calc(100vh - 64px);
 
-  @apply relative min-w-40 bg-red-300;
-}
+    @apply relative min-w-40 bg-red-300;
 
-.column-right {
-  height: calc(100vh - 64px);
+    .resize-save {
+      position: absolute;
+      top: 0;
+      right: 5px;
+      bottom: 0;
+      left: 0;
+      padding: 16px;
+      overflow-x: hidden;
+    }
 
-  @apply flex items-center justify-center flex-1 p-4 min-w-40 bg-blue-300 overflow-hidden;
-}
+    /* 拖拽线 */
+    .resize-line {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      pointer-events: none;
+      border-right: 4px solid gray;
+      border-left: 4px solid gray;
+      transition: border 0.3s ease-in-out;
 
-.resize-save {
-  position: absolute;
-  top: 0;
-  right: 5px;
-  bottom: 0;
-  left: 0;
-  padding: 16px;
-  overflow-x: hidden;
-}
+      &::before {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        display: flex;
+        flex-direction: column-reverse;
+        height: 8px;
+        color: transparent;
+        content: '·';
+        transform: translate(-50%, -50%) scale(2.5);
+        transition: color 0.3s ease-in-out;
+      }
+    }
 
-.resize-bar {
-  height: inherit;
-  overflow: scroll;
-  cursor: ew-resize;
-  cursor: col-resize;
-  opacity: 0;
-  resize: horizontal;
-}
+    .resize-bar {
+      height: inherit;
+      overflow: scroll;
+      cursor: ew-resize;
+      cursor: col-resize;
+      opacity: 0;
+      resize: horizontal;
 
-/* 拖拽线 */
-.resize-line {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  pointer-events: none;
-  border-right: 1px solid #bbb;
-  border-left: 1px solid #bbb;
-}
+      &:hover ~ .resize-line {
+        border-right: 4px solid black;
+        border-left: 4px solid black;
+        transition: border 0.3s ease-in-out;
 
-.resize-bar:hover ~ .resize-line,
-.resize-bar:active ~ .resize-line {
-  border-left: 1px dashed black;
-}
+        &::before {
+          color: white;
+          transition: color 0.3s ease-in-out;
+        }
+      }
 
-.resize-bar::-webkit-scrollbar {
-  width: 200px;
-  height: inherit;
+      &:active ~ .resize-line {
+        border-right: 4px solid black;
+        border-left: 4px solid black;
+        transition: border 0.3s ease-in-out;
+
+        &::before {
+          color: white;
+          transition: color 0.3s ease-in-out;
+        }
+      }
+
+      &::-webkit-scrollbar {
+        width: 200px;
+        height: inherit;
+      }
+    }
+  }
+
+  .column-right {
+    height: calc(100vh - 64px);
+
+    @apply flex items-center justify-center flex-1 p-4 min-w-40 bg-blue-300 overflow-hidden;
+  }
 }
 </style>
