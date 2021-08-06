1a6b20e8256435cdcb5e99250f6f630f3b097542
zhixiangyao <blueskyzx@icloud.com>
2021-08-06 22:09:58 +0800

    fix: 类型优化

--- src/plugins/Aplayer/index.d.ts
+++ src/plugins/Aplayer/index.d.ts
@@ -1,5 +1,25 @@
 declare module 'APlayer' {
-  const APlayer: any
+  interface Audio {
+    name: string
+    artist: string
+    url: string
+    lrc: string
+    cover: string
+  }
+
+  interface APlayer {
+    new (target: {
+      container: any
+      fixed?: boolean
+      mini?: boolean
+      volume: number
+      theme: string
+      lrcType: number
+      audio: Audio[]
+    }): any
+  }
+
+  const APlayer: APlayer
 
   export { APlayer as default }
 }
