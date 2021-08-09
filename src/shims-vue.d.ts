d53ec78176eb1ac316902ec28f2599b8b78c2dbf
zhixiangyao <blueskyzx@icloud.com>
2021-08-09 14:53:29 +0800

    type: test

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,17 @@
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
+
+  global {
+    const graphql: (query: TemplateStringsArray) => void
+    namespace TestNamespace {
+      interface ComponentProps {
+        children: {}
+        data: {}
+      }
+    }
+  }
+
   export default component
 }
 
