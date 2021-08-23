9f598de4b7173ab95aeb22d933d69cd901ca43d3
zhixiangyao <blueskyzx@icloud.com>
2021-08-23 23:27:27 +0800

    fix

--- src/layouts/components/Main.vue
+++ src/layouts/components/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex flex-col justify-center items-center dark:bg-black min-h-screen pt-16;">
+  <main class="flex flex-col justify-center items-center dark:bg-black min-h-screen pt-16">
     <slot name="default" />
   </main>
 </template>
