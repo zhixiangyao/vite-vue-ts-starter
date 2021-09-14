accdc55c5284f56d8dfd42e184422ed707e1920c
zhixiangyao <blueskyzx@icloud.com>
2021-09-14 16:17:27 +0800

    fix

--- src/layouts/components/Nav.vue
+++ src/layouts/components/Nav.vue
@@ -7,7 +7,7 @@ export default defineComponent({
     const slots = useSlots()
 
     return () => (
-      <nav class="nav bg-gray-700 dark:bg-black fixed top-0 left-0 right-0 z-10">
+      <nav class="nav bg-gray-700 dark:bg-black fixed top-0 left-0 w-screen z-10">
         <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
           <div class="relative flex items-center justify-between h-16 overflow-x-auto overflow-y-hidden">
             <div class="text-green-600 flex-shrink-0 px-3 py-2 rounded-md text-sm font-medium">
