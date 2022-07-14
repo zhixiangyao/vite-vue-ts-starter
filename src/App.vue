ac4bcbe5307ea77cee5c666dcbeeef40e6aefed1
zhixiangyao <blueskyzx@icloud.com>
2022-07-14 16:38:56 +0800

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
-<script lang="ts" setup>
+<script lang="tsx">
+import { defineComponent, Teleport } from 'vue'
+import { RouterView } from 'vue-router'
 import APlayer from '/@/plugins/Aplayer/index.vue'
 import Github from '/@/plugins/Github.vue'
 import Live2dWidget from '/@/plugins/Live2dWidget.vue'
+
+function App() {
+  return () => (
+    <>
+      <RouterView />
+
+      <Teleport to="#global-components">
+        <APlayer />
+
+        <Live2dWidget />
+
+        <Github class="hidden xl:block" href="https://github.com/zhixiangyao/vite-vue-ts-starter" />
+      </Teleport>
+    </>
+  )
+}
+
+export default defineComponent(App)
 </script>
-
-<template>
-  <router-view />
-
-  <teleport to="#global-components">
-    <APlayer />
-
-    <Live2dWidget />
-
-    <Github class="hidden xl:block" href="https://github.com/zhixiangyao/vite-vue-ts-starter" />
-  </teleport>
-</template>
