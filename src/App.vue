eeba1884a009d00740e0f97498e0ddb0def48608
zhixiangyao <blueskyzx@icloud.com>
2022-12-15 10:45:24 +0800

    delete: Remove `APlayer`

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script lang="tsx">
 import { defineComponent, Teleport } from 'vue'
 import { RouterView } from 'vue-router'
-import APlayer from '/@/plugins/Aplayer/index.vue'
 import Github from '/@/plugins/Github.vue'
 import Live2dWidget from '/@/plugins/Live2dWidget.vue'
 
@@ -11,8 +10,6 @@ function App() {
       <RouterView />
 
       <Teleport to="#global-components">
-        <APlayer />
-
         <Live2dWidget />
 
         <Github class="hidden xl:block" href="https://github.com/zhixiangyao/vite-vue-ts-starter" />
